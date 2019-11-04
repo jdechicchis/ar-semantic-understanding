74d8b808888f162b238330a1766841bf9b478ef8
Joseph DeChicchis <josephdechicchis@gmail.com>
2019-11-03 19:16:51 -0500

    Add seg mask

--- visualize_data.py
+++ visualize_data.py
@@ -6,10 +6,120 @@ import sys
 import os
 import argparse
 import json
+import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 
 PLOT_COLORS = ["blue", "green", "red", "cyan", "magenta", "yellow"]
+
+OBJECT_COLORS = {
+    "airconditioner": [1.0, 1.0, 1.0],
+    "airduct": [1.0, 1.0, 1.0],
+    "air_vent": [1.0, 1.0, 1.0],
+    "bag": [1.0, 1.0, 1.0],
+    "ball": [1.0, 1.0, 1.0],
+    "basket": [1.0, 1.0, 1.0],
+    "books": [1.0, 1.0, 1.0],
+    "bookshelf": [1.0, 1.0, 1.0],
+    "bottle": [1.0, 1.0, 1.0],
+    "bowl": [1.0, 1.0, 1.0],
+    "box": [1.0, 1.0, 1.0],
+    "blinds": [1.0, 1.0, 1.0],
+    "bulletin_board": [1.0, 1.0, 1.0],
+    "cabinet": [1.0, 1.0, 1.0],
+    "cablemodem": [1.0, 1.0, 1.0],
+    "cable_modem": [1.0, 1.0, 1.0],
+    "cablerack": [1.0, 1.0, 1.0],
+    "camera": [1.0, 1.0, 1.0],
+    "ceiling": [1.0, 1.0, 1.0],
+    "chair": [1.0, 1.0, 1.0],
+    "clipboard": [1.0, 1.0, 1.0],
+    "clock": [1.0, 1.0, 1.0],
+    "column": [1.0, 1.0, 1.0],
+    "computer": [1.0, 1.0, 1.0],
+    "cone": [1.0, 1.0, 1.0],
+    "counter": [1.0, 1.0, 1.0],
+    "cup": [1.0, 1.0, 1.0],
+    "curtain": [1.0, 1.0, 1.0],
+    "desk": [1.0, 1.0, 1.0],
+    "doll": [1.0, 1.0, 1.0],
+    "door": [1.0, 1.0, 1.0],
+    "dishwasher": [1.0, 1.0, 1.0],
+    "divider": [1.0, 1.0, 1.0],
+    "electrical_outlet": [1.0, 1.0, 1.0],
+    "envelope": [1.0, 1.0, 1.0],
+    "eraser": [1.0, 1.0, 1.0],
+    "exit_sign": [1.0, 1.0, 1.0],
+    "flowers": [1.0, 1.0, 1.0],
+    "garbage_bin": [1.0, 1.0, 1.0],
+    "handsanitizer": [1.0, 1.0, 1.0],
+    "hanginghooks": [1.0, 1.0, 1.0],
+    "heater": [1.0, 1.0, 1.0],
+    "hole_puncher": [1.0, 1.0, 1.0],
+    "fan": [1.0, 1.0, 1.0],
+    "faucet": [1.0, 1.0, 1.0],
+    "fax_machine": [1.0, 1.0, 1.0],
+    "file": [1.0, 1.0, 1.0],
+    "fire_place": [1.0, 1.0, 1.0],
+    "floor": [1.0, 1.0, 1.0],
+    "folder": [1.0, 1.0, 1.0],
+    "fridge": [1.0, 1.0, 1.0],
+    "hook": [1.0, 1.0, 1.0],
+    "jar": [1.0, 1.0, 1.0],
+    "keyboard": [1.0, 1.0, 1.0],
+    "ladder": [1.0, 1.0, 1.0],
+    "laptop": [1.0, 1.0, 1.0],
+    "light": [1.0, 1.0, 1.0],
+    "magazine": [1.0, 1.0, 1.0],
+    "magnet": [1.0, 1.0, 1.0],
+    "mantle": [1.0, 1.0, 1.0],
+    "map": [1.0, 1.0, 1.0],
+    "microwave": [1.0, 1.0, 1.0],
+    "monitor": [1.0, 1.0, 1.0],
+    "mouse": [1.0, 1.0, 1.0],
+    "paper": [1.0, 1.0, 1.0],
+    "paper_cutter": [1.0, 1.0, 1.0],
+    "papertowel": [1.0, 1.0, 1.0],
+    "paperrack": [1.0, 1.0, 1.0],
+    "papertoweldispenser": [1.0, 1.0, 1.0],
+    "pen": [1.0, 1.0, 1.0],
+    "pencil_holder": [1.0, 1.0, 1.0],
+    "picture": [1.0, 1.0, 1.0],
+    "pipe": [1.0, 1.0, 1.0],
+    "plant": [1.0, 1.0, 1.0],
+    "pot": [1.0, 1.0, 1.0],
+    "printer": [1.0, 1.0, 1.0],
+    "projector": [1.0, 1.0, 1.0],
+    "projector_screen": [1.0, 1.0, 1.0],
+    "ruler": [1.0, 1.0, 1.0],
+    "scissor": [1.0, 1.0, 1.0],
+    "screen": [1.0, 1.0, 1.0],
+    "shelf": [1.0, 1.0, 1.0],
+    "shelves": [1.0, 1.0, 1.0],
+    "sink": [1.0, 1.0, 1.0],
+    "sofa": [1.0, 1.0, 1.0],
+    "speaker": [1.0, 1.0, 1.0],
+    "stamp": [1.0, 1.0, 1.0],
+    "stand": [1.0, 1.0, 1.0],
+    "stapler": [1.0, 1.0, 1.0],
+    "stereo": [1.0, 1.0, 1.0],
+    "stoveburner": [1.0, 1.0, 1.0],
+    "styrofoamobject": [1.0, 1.0, 1.0],
+    "table": [1.0, 1.0, 1.0],
+    "tape": [1.0, 1.0, 1.0],
+    "tape_dispenser": [1.0, 1.0, 1.0],
+    "telephone": [1.0, 1.0, 1.0],
+    "telephonecord": [1.0, 1.0, 1.0],
+    "thermostat": [1.0, 1.0, 1.0],
+    "vase": [1.0, 1.0, 1.0],
+    "wall": [1.0, 1.0, 1.0],
+    "watercarboy": [1.0, 1.0, 1.0],
+    "water_purifier": [1.0, 1.0, 1.0],
+    "window": [1.0, 1.0, 1.0],
+    "wire": [1.0, 1.0, 1.0],
+    "whiteboard": [1.0, 1.0, 1.0],
+    "unknown": [0.0, 0.0, 0.0],
+}
 
 class Visualizer:
     """
@@ -41,10 +151,18 @@ class Visualizer:
         Display the image with segmentation ground truth.
         """
         image = plt.imread(self.__image_file)
-        fig, ax = plt.subplots()
+        plt.figure()
+        ax = plt.subplot(1, 2, 1)
+        #fig, ax = plt.subplots()
         ax.imshow(image)
         ax.axis("off")
         annotations = self.__read_annotation_file()
+
+        print(np.shape(image))
+        image_height = np.shape(image)[0]
+        image_width = np.shape(image)[1]
+
+        segmented_image = np.zeros(np.shape(image))
 
         color_idx = 0
         color_assignments = {}
@@ -64,9 +182,26 @@ class Visualizer:
                                     alpha=0.5)
             ax.add_patch(patch)
 
+            x, y = np.meshgrid(np.arange(image_width), np.arange(image_height))
+            x, y = x.flatten(), y.flatten()
+            points = np.vstack((x, y)).T
+            segment_path = patches.Polygon(annotation["coordinates"])
+            grid = segment_path.contains_points(points, radius=1e-9)
+            mask = grid.reshape(image_height, image_width)
+
+            #TODO: Use mask to generate segment class for each pixel
+
+            for h in range(0, image_height):
+                for w in range(0, image_width):
+                    if mask[h][w]:
+                        segmented_image[h][w] = OBJECT_COLORS[annotation["name"]]
+
         handles, labels = ax.get_legend_handles_labels()
 
         plt.legend(handles=handles, ncol=2, bbox_to_anchor=(1.0, 1.0))
+
+        ax = plt.subplot(1, 2, 2)
+        ax.imshow(segmented_image)
 
         plt.show()
 
